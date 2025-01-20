c03fe349bb871fcaf9a55fdba81d0ac29eb721ab
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2025-01-20 14:51:10 +0900

    Finish Testimonials

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,8 +10,13 @@ const router = createRouter({
       component: ButtonView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/button',
+      name: 'button',
+      component: ButtonView,
+    },
+    {
+      path: '/testimonials',
+      name: 'testimonials',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
